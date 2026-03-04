67ffa341d5b604895d43d608f4bfe95748b7cbb6
John Galt <johngaltfirstrun@gmail.com>
2026-03-04 12:42:10 -0500

    waffle: revert to prebuilt audio
    Some OP12 variants on stock do not use lvacfs, and when used here
    counterintuitively causes mic volume to be too low. Stock hal has some
    behavior which resolves this, so when this behavior is implemented,
    this will be reverted later.

--- device.mk
+++ device.mk
@@ -16,9 +16,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    $(LOCAL_PATH)/configs/audio/resourcemanager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/resourcemanager_pineapple_mtp.xml \
-    $(LOCAL_PATH)/configs/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
+    $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2376
