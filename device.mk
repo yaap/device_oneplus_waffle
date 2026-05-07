0e63e8df95718e47e9d906f2b0bb1e24e4067f6c
John Galt <johngaltfirstrun@gmail.com>
2026-05-07 16:16:37 -0400

    waffle: enable oplus anchor list

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,surfaceflinger,arr_use_oplus_ltpo_rates,true)
 $(call soong_config_set,qtidisplay,sm8650_pxlw_vendor_namespace,vendor/oneplus/waffle)
 $(call soong_config_set,qtidisplay,sm8650_pxlw_hw_iris7,true)
 
