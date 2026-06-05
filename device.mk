29b09099defe175e7d3dd9e4440ed0ba1cd26387
John Galt <johngaltfirstrun@gmail.com>
2026-06-05 10:25:25 -0400

    waffle: bump min refresh rate to 10hz
    As idle policy is now applied properly on vrr paths in surfaceflinger[1],
    and it can drop as low as kMinSupportedFrameRate[2], it has been noted
    that switching to/from 1hz has significant jank. Whereas switching
    to/from 10hz has no jank.
    
    Previously while 1hz was in the anchor list, the anchor list was only
    used for content matching (such as switching to 24hz on 24fps media), so
    we never noticed the 1hz jank.
    
    [1]: https://github.com/YAAP/frameworks_native/commit/a91cf42d5d040f6a1b92ae62ab7d09a3a107d5ce
    [2]: https://github.com/YAAP/frameworks_native/commit/c34c4ecb5cea115feb2583aad974e0f8e111c8e0

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 # Display
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
-$(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,10)
 $(call soong_config_set,surfaceflinger,arr_use_oplus_ltpo_rates,true)
 $(call soong_config_set,qtidisplay,sm8650_pxlw_vendor_namespace,vendor/oneplus/waffle)
 $(call soong_config_set,qtidisplay,sm8650_pxlw_hw_iris7,true)
